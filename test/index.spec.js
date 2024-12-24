2adc01054fbc0fe0e774095a680e0d5b20c40571
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2024-12-24 18:06:10 +0100

    Misc fixes

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,5 @@
 import { transformFileSync } from '@babel/core';
 import { expect } from 'chai';
-import path from 'path';
 import fs from 'fs';
 import Vinyl from 'vinyl';
 import gulpBabel from 'gulp-babel';
@@ -13,15 +12,15 @@ describe('transforms assets', () => {
   before(() => {
     const ofs = { ...fs };
     vol.fromJSON({
-      './node_modules/fake-module/file.txt': fs.readFileSync(path.join(__dirname, './file.txt'), 'utf-8'),
+      './node_modules/fake-module/file.txt': fs.readFileSync(new URL(import.meta.resolve('./file.txt')), 'utf-8'),
     });
-    vol.writeFileSync('./node_modules/fake-module/logo.png', fs.readFileSync(path.join(__dirname, 'fixtures/logo.png')));
+    vol.writeFileSync('./node_modules/fake-module/logo.png', fs.readFileSync(new URL(import.meta.resolve('./fixtures/logo.png'))));
     ufs.use(memfs).use(ofs);
     patchFs(ufs);
   });
 
   const transform = (filename, config = {}) => transformFileSync(
-    path.resolve(__dirname, filename),
+    new URL(import.meta.resolve(filename)).pathname,
     {
       babelrc: false,
       presets: ['@babel/env'],
@@ -35,7 +34,7 @@ describe('transforms assets', () => {
     ['located within a Node.js module', '-from-node-module'],
   ]).describe('when asset is %s', (description, suffix) => {
     it('replaces require statements with filename', () => {
-      expect(transform(`fixtures/require-txt${suffix}.js`, {
+      expect(transform(`./fixtures/require-txt${suffix}.js`, {
         extensions: ['txt'],
       }).code).to.be.equal(`"use strict";
 
@@ -43,7 +42,7 @@ var file = "file.txt?vmiIOMq";`);
     });
 
     it('replaces import statements with filename', () => {
-      expect(transform(`fixtures/import-txt${suffix}.js`, {
+      expect(transform(`./fixtures/import-txt${suffix}.js`, {
         extensions: ['txt'],
       }).code).to.be.equal(`"use strict";
 
@@ -52,7 +51,7 @@ function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default":
     });
 
     it('replaces import statements with filename and then exports', () => {
-      expect(transform(`fixtures/import-export-txt${suffix}.js`, {
+      expect(transform(`./fixtures/import-export-txt${suffix}.js`, {
         extensions: ['txt'],
       }).code).to.be.equal(`"use strict";
 
@@ -89,29 +88,29 @@ function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e
       stream.on('end', cb);
 
       stream.write(new Vinyl({
-        cwd: __dirname,
-        base: path.join(__dirname, 'fixtures'),
-        path: path.join(__dirname, `fixtures/import-txt${suffix}.js`),
-        contents: fs.readFileSync(path.join(__dirname, `fixtures/import-txt${suffix}.js`)),
+        cwd: new URL(import.meta.resolve('.')).pathname,
+        base: new URL(import.meta.resolve('./fixtures')).pathname,
+        path: new URL(import.meta.resolve(`./fixtures/import-txt${suffix}.js`)).pathname,
+        contents: fs.readFileSync(new URL(import.meta.resolve(`./fixtures/import-txt${suffix}.js`))),
       }));
 
       stream.end();
     });
 
     it('removes global requires', () => {
-      expect(transform(`fixtures/empty-require${suffix}.js`, {
+      expect(transform(`./fixtures/empty-require${suffix}.js`, {
         extensions: ['txt'],
       }).code).to.be.equal('"use strict";');
     });
 
     it('fixtures/empty-import.js', () => {
-      expect(transform(`fixtures/empty-import${suffix}.js`, {
+      expect(transform(`./fixtures/empty-import${suffix}.js`, {
         extensions: ['txt'],
       }).code).to.be.equal('"use strict";');
     });
 
     it('fixtures/png-import.js with [contenthash] in name', () => {
-      expect(transform(`fixtures/png-import${suffix}.js`, {
+      expect(transform(`./fixtures/png-import${suffix}.js`, {
         extensions: ['png'],
         name: '/images/[contenthash].[ext]',
       }).code).to.be.equal(`"use strict";
